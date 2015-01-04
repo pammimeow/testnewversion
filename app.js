2d5ad6d1a2311bc3450c79125911c8b3979a1d17
riteshbabu <sendritesh111@gmail.com>
2015-01-04 15:11:44 +0530

    changes masde in app.js for dev branch

--- app.js
+++ app.js
@@ -170,7 +170,7 @@ app.get('/edit/:id', function(req, res) {
 
 
 http.createServer(app).listen(3000, function() {
- console.log("Biz vid app started");
+ console.log("Biz vid app started at port number 3000");
 });
 
 
